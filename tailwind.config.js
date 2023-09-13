8cab707d5f956f29673dfe711ca5c399142e86b7
NotScorpio91 <notscorpio91@gmail.com>
2023-09-13 13:28:58 +0500

    customized design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+       inter: ["inter"],
+       blinker: ["blinker"],
+       roboto: ["roboto"]
+      }
+    },
   },
   plugins: [],
 }
