e7d90e57209a16451df7d7c616ba3f47fe53407f
notscorpio91 <notscorpio91@gmail.com>
2023-09-21 01:23:25 +0500

    Mobile hamburger completely update

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,6 +31,13 @@ module.exports = {
        roboto: ["roboto"]
       }
     },
+    variants:{
+      extend:{
+        display: ['group-focus'],
+        opacity: ['group-focus'],
+        inset: ['group-focus']
+      },
+    },
   },
   plugins: [],
 }
